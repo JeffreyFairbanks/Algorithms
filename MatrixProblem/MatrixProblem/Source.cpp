a2b91cb6b5491ac9acd2c02c0d0032c383ac07f2
Jeffrey Fairbanks <jfairbanks@nnu.com>
2021-02-17 13:43:14 -0700

    We now have an outline of a solution. Need to make the cmd line parameters start to work as well as optimize some things.

--- MatrixProblem/MatrixProblem/Source.cpp
+++ MatrixProblem/MatrixProblem/Source.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
+#include<iomanip>
 #include<fstream>
 #include<string>
+#include<stdio.h>
+#include<limits.h>
+  
 
 using namespace std;
+
+
+/* A naive recursive implementation that simply
+follows the above optimal substructure property */
+
+
 
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 /*::  Function Prototypes                                     ::*/
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-
+int MatrixChainOrder(int p[], int i, int j);
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +28,13 @@ int main(int argc, char* argv[])
 		// Do something if the second arguement is stated, like open a file.
 		fstream file(argv[1]); //Argv[1] is the name if the file.
 		if (file.is_open()) {
-			//Process the file here.
+          			// Where we actually do the work :) 
+                    int arr[] = { 1, 2, 3, 4, 3 };               //NEED TO HAVE THEM ACTUALLY PASS IN THE VALUES FROM THE CMD LINE
+                    int n = sizeof(arr) / sizeof(arr[0]);        //MORE CMD LINE STUFFS
+
+                    std::cout << "Minimum number of multiplications is %d ", MatrixChainOrder(arr, 1, n - 1);
+                    cin.get();
+
 			file.close();
 		}
 		else {
@@ -29,9 +45,34 @@ int main(int argc, char* argv[])
 	}
 }
 
-
-
-
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 /*::  Functions                                               ::*/
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+   //TAKEN WITH HELP FROM https://www.wikitechy.com/technology/python-programming-matrix-chain-multiplication/
+  // Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
+int MatrixChainOrder(int p[], int i, int j)
+{
+  if (i == j)
+    return 0;
+  int k;
+  int min = INT_MAX;
+  int count;
+
+  // place parenthesis at different places between first
+  // and last matrix, recursively calculate count of
+  // multiplications for each parenthesis placement and
+  // return the minimum count
+  for (k = i; k < j; k++)
+  {
+    count = MatrixChainOrder(p, i, k) +
+      MatrixChainOrder(p, k + 1, j) +
+      p[i - 1] * p[k] * p[j];
+
+    if (count < min)
+      min = count;
+  }
+
+  // Return minimum count
+  return min;
+}
