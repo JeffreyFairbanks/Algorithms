e921b63a7386a00d67bc53972d9e0da4a9241ac9
TonicHex <49658461+JeffreyFairbanks@users.noreply.github.com>
2021-04-10 10:52:21 -0600

    Done, but needs to be cleaned and commented.

---
+++ ConnectComponentsGood/ConnectComponentsGood/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace ConnectComponentsGood
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main(String[] args)
+    {
+      try
+      {
+        if (args.Length > 0)
+        {
+          string filename = args[0];
+
+          StreamReader reader = new StreamReader(filename);
+          string json = reader.ReadToEnd();
+          reader.Close();
+
+          var constraints = JsonConvert.DeserializeObject<Root>(json);
+
+          var dict = new Dictionary<int, string>();
+
+          for(int i = 0; i < constraints.CountyNames.Count; i++)
+          {
+            dict.Add(i, constraints.CountyNames[i]);
+          }
+
+          int vertices = constraints.CountyNames.Count;
+          var edges = constraints.FindEdges();
+          int edgeCount = edges.Count;
+
+          Graph graph = new Graph(vertices, edgeCount, dict);
+
+          for(int i = 0; i < edgeCount; i++)                        //populate the edges of the graph 
+          {
+            graph.edge[i].src = edges[i].Key;
+            graph.edge[i].dest = edges[i].Value;
+            graph.edge[i].weight = 1;
+          }                                                          //building the graph
+
+
+          // Function call
+          graph.KruskalMST();
+        }
+
+      } 
+      catch
+      {
+        Console.WriteLine("error message :( \n");
+      }
+      Console.ReadLine();
+    }
+  }
+}
+
+
+
+
+
+
+
+
---
+++ ConnectComponentsGood/ConnectComponentsGood/Root.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectComponentsGood
+{
+  public class Root
+  {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public List<string> CountyNames { get; set; }
+    public List<string> Domain { get; set; }
+    public List<List<int>> Graph { get; set; }
+
+
+  public List<KeyValuePair<int, int>> FindEdges()
+  {
+
+      var edges = new List<KeyValuePair<int, int>>();
+
+      for(int i = 0; i < Graph.Count; i++)
+      {
+        for(int j = 0; j < Graph[i].Count; j++)
+        {
+          if(Graph[i][j] == 1)
+          {
+            var pair = new KeyValuePair<int, int>(i, j);
+            var pair2 = new KeyValuePair<int, int>(j, i);
+
+            if (!edges.Contains(pair) && !edges.Contains(pair2)) //check if edge has been accounted for
+              edges.Add(pair);
+
+          }
+        }
+      }
+
+      return edges;
+  }
+
+  }
+
+}
