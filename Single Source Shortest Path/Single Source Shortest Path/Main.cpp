6463d510d1099339b474d7bfb9f1d23e024ac9c3
TonicHex <49658461+JeffreyFairbanks@users.noreply.github.com>
2021-04-26 17:23:10 -0600

    keep this one

--- Single Source Shortest Path/Single Source Shortest Path/Main.cpp
+++ Single Source Shortest Path/Single Source Shortest Path/Main.cpp
@@ -1,4 +1,5 @@
 // JEFFREY FAIRBANKS
+#define  _CRT_SECURE_NO_WARNINGS
 
 #include<iostream>
 #include<vector>
@@ -7,6 +8,8 @@
 #include<algorithm>
 #include <limits.h>
 #include <stdio.h>
+
+#include "Dijkstra.h"
 
 using namespace std;
 
@@ -20,20 +23,25 @@ using namespace std;
 
 
 // Number of vertices in the graph
-#define V 9
+#define NODES_MAX 100
+#define FILE_MAX 8192
+#define INFINITY 9999
+
 
 
 ///////////////////////
 //    PROTOTYPES     //
 ///////////////////////
-int minDistance(int dist[], bool sptSet[]);
-void printSolution(int dist[], int n);
-void dijkstra(int graph[V][V], int src);
+int input_from_file(char* filename, struct Dijkstra_input* input);
+void print_dijkstra_ouput(struct Dijkstra_output* output);
+string Semicolin2Space(string text);
 
 
 //Where all the fun begins 
 int main(int argc, char* argv[])
 {
+	struct Dijkstra_input input;
+
 	try {
 		if (argc < 3) // we need 4 files in order to run this program.
 			// Tell the user what went wrong. 
@@ -52,10 +60,21 @@ int main(int argc, char* argv[])
 				//this will read in the header line 
 				getline(file, headers, '\n');
 
-				//this will get the first letter that is read in on each line 
-				file >> temp;
+				////this will get the first letter that is read in on each line 
+				//file >> temp;
 
 
+
+				input_from_file(argv[1], &input);
+				input.starting_node = atoi(argv[2]);
+				input.target_node = atoi(argv[3]);
+
+				struct Dijkstra_output output;
+				output = dijkstra(&input);
+
+				print_dijkstra_ouput(&output);
+
+				cin.get();
 
 			}
 			else
@@ -76,99 +95,49 @@ int main(int argc, char* argv[])
 }
 
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
-int minDistance(int dist[], bool sptSet[])
-{
-	// Initialize min value
-	int min = INT_MAX, min_index;
+int input_from_file(char* filename, struct Dijkstra_input* input) {
+	char buffer[FILE_MAX];
+	char* record, * line;
+	FILE* fstream;
+	int i = 0, j = 0;
+	int columns = 0;
+	bool columns_found = false;
 
-	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
-
-	return min_index;
-}
-
-// A utility function to print the constructed distance array
-void printSolution(int dist[], int n)
-{
-	printf("Vertex Distance from Source\n");
-	for (int i = 0; i < V; i++)
-		printf("%d tt %d\n", i, dist[i]);
-}
-
-// Function that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
-{
-	int dist[V]; // The output array. dist[i] will hold the shortest
-	// distance from src to i
-
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-	// path tree or shortest distance from src to i is finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as false
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
-
-	// Distance of source vertex from itself is always 0
-	dist[src] = 0;
-
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in the first iteration.
-		int u = minDistance(dist, sptSet);
-
-		// Mark the picked vertex as processed
-		sptSet[u] = true;
-
-		// Update dist value of the adjacent vertices of the picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet, there is an edge from
-			// u to v, and total weight of path from src to v through u is
-			// smaller than current value of dist[v]
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+	fstream = fopen(filename, "r");
+	if (fstream == NULL) {
+		printf("\nFile opening failed when reading input.");
+		return -1;
 	}
 
-	// print the constructed distance array
-	printSolution(dist, V);
+
+	while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL) {
+		record = strtok(line, " ");
+		while (record != NULL) {
+			input->adjacency[i][j] = atoi(record);
+			j++;
+			record = strtok(NULL, " ");
+		}
+		if (!columns_found) {
+			columns = input->nodes = j;
+			columns_found = true;
+		}
+		else if (j < columns)
+			printf("\nLess columns than %d in %dª row, they will be treated as 0", columns, i + 1);
+		else if (j > columns)
+			printf("\nMore columns than %d in %dª row, the rest will be ignored", columns, i + 1);
+		j = 0;
+		++i;
+	}
+
+	return 1;
+}
+
+void print_dijkstra_ouput(struct Dijkstra_output* output) {
+	printf("\nThe Dijkstra shortest path is: %d", output->path[0]);
+	for (int i = 1; i < output->size; i++) {
+		printf("-->%d", output->path[i]);
+	}
+	printf("\nPath total distance: %d", output->distance);
 }
 
 
-
-//// driver program to test above function
-//int main()
-//{
-//	/* Let us create the example graph discussed above */
-//	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-//											{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-//											{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-//											{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-//											{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-//											{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-//											{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-//											{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-//											{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-//
-//	dijkstra(graph, 0);
-//
-//	return 0;
-//}
-//
-//
-//
-//a  b  c  d  e  f  g  h  i
-//a - 1  4 - 1 - 1 - 1 - 1 - 1  8 - 1
-//b  4 - 1  8 - 1 - 1 - 1 - 1 11 - 1
-//c - 1  8 - 1  7 - 1  4 - 1 - 1  2
-//d - 1 - 1  7 - 1  9 14 - 1 - 1 - 1
-//e - 1 - 1 - 1  9 - 1 10 - 1 - 1 - 1
-//f - 1 - 1  4 14 10 - 1  2 - 1 - 1
-//g - 1 - 1 - 1 - 1 - 1  2 - 1  1  6
-//h  8 11 - 1 - 1 - 1 - 1  1 - 1  7
-//i - 1 - 1  2 - 1 - 1 - 1  6  7 - 1
